5c4b4ed3ed4356d1b152fc5f9c058b96a1479c70
danielprofili <dprofili@gmail.com>
2018-05-29 13:49:35 -0400

    Append xyz, gzmat, and charges in that order

--- nn_input.sh
+++ nn_input.sh
@@ -8,15 +8,22 @@
 
 dest_dir="/nn"
 working_dir=$(pwd)
+# initialize the input file
+echo "BEGIN INPUT FILE" > $working_dir/input
 cd subs
 for f in *
 do
 # f is the folder (i.e. the perturbation name TFSI_xxx_xx)
-    cd $f 
     #echo $f 
-    echo "$f\n" >> $working_dir/input
-    cat $f.charges >> $working_dir/input
+    # Print the name at the top of the block
     cd $working_dir
+    echo -e "\n$f\n" >> $working_dir/input
     cat xyz/$f.xyz >> $working_dir/input
+    echo -e "\n===================\n" >> $working_dir/input
     cat gzmat/$f.gzmat >> $working_dir/input
+    echo -e "\n===================\n" >> $working_dir/input
+    cat $working_dir/subs/$f/$f.charges >> $working_dir/input
+    echo -e "\n===================\n" >> $working_dir/input
+    #echo DEBUG
+    #sleep 5
 done
