bc29ee5ad720b1b4db44b39579da5ddbdf4ec702
danielprofili <dprofili@gmail.com>
2018-05-29 12:49:55 -0400

    Comment nn_input.sh

--- nn_input.sh
+++ nn_input.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+# nn_input
+#
+# Generates the neural network input files containing xyz, gzmat, and charge data
+# for each perturbation
+# 
+# 29 May 2018
 for f in subs/*
 do
 # f is the folder
