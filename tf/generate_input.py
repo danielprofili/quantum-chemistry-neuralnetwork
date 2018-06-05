a881d35d194e94bf0c8c0b392405efbda4cba128
danielprofili <dprofili@gmail.com>
2018-06-05 11:37:06 -0400

    Add regex to generate_input

--- tf/generate_input.py
+++ tf/generate_input.py
@@ -1,1 +1,13 @@
 # Generate the input vector from the input file
+import numpy
+import argparse
+import re
+
+# handle input argument - the input file path
+parser = argparse.ArgumentParser()
+parser.add_argument("inputfile", help="path to the input file")
+parser.parse_args()
+
+# set up regex parsing object for parsing the molecule name (TFSI_###_###)
+reg = re.compile('TFSI_[0-9]{1,3}_[0-9]{1,3}')
+
