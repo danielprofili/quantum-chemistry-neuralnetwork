2483b1b0a65e0b3ac69e726f51608712c17c9110
danielprofili <dprofili@gmail.com>
2018-06-05 11:55:33 -0400

    Match regex in input file

--- tf/generate_input.py
+++ tf/generate_input.py
@@ -1,4 +1,5 @@
 # Generate the input vector from the input file
+# 5 June 2018
 import numpy
 import argparse
 import re
@@ -6,8 +7,15 @@ import re
 # handle input argument - the input file path
 parser = argparse.ArgumentParser()
 parser.add_argument("inputfile", help="path to the input file")
-parser.parse_args()
+args = parser.parse_args()
 
 # set up regex parsing object for parsing the molecule name (TFSI_###_###)
 reg = re.compile('TFSI_[0-9]{1,3}_[0-9]{1,3}')
 
+f = open(args.inputfile)
+line = ''
+while (not reg.match(line)):
+    line = f.readline()
+
+# DEBUG:
+print(line)
