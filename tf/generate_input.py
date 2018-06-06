a6970b887449367b60c9f995b77159d3f4e1531f
danielprofili <dprofili@gmail.com>
2018-06-06 17:02:26 -0400

    Correctly get doubles from xyz block

--- tf/generate_input.py
+++ tf/generate_input.py
@@ -9,16 +9,31 @@ parser = argparse.ArgumentParser()
 parser.add_argument("inputfile", help="path to the input file")
 args = parser.parse_args()
 
-# set up regex parsing object for parsing the molecule name (TFSI_###_###)
-reg = re.compile('TFSI_[0-9]{1,3}_[0-9]{1,3}')
+def get_coords(xyz_mat):
+   print("asdasd") 
 
+# set up regex parsing object for parsing the molecule name (TFSI_###_###)
+reg = re.compile('TFSI_[0-9]{1,3}_[0-9]{1,3}\.gzmat')
 f = open(args.inputfile)
 line = f.readline() 
-while (line != ''):
-    while (not reg.match(line)):
-        line = f.readline()
+while (not reg.match(line) and f != ''):
+    line = f.readline()
 
-# DEBUG:
-print(line)
+# now at the line containing the xyz
+# TODO DEBUG:
+# print(line)
+line = f.readline()
+xyz = ''
+while '==================' not in line:
+    xyz += line
+    line = f.readline()
 
+# print(xyz)
+#print(re.findall('-?\d\.\d*', xyz))
+nums = [float(s) for s in re.findall('-?\d\.\d*', xyz)]
+# print(nums)
+for i in range(0, len(nums)):
+    print(nums[i])
 f.close()
+
+
