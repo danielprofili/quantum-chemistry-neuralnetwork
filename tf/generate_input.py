107bfd2ed3e42986fe7354919954797ed6985c2c
danielprofili <dprofili@gmail.com>
2018-06-06 20:14:31 -0400

    Calculate pairwise distances between points

--- tf/generate_input.py
+++ tf/generate_input.py
@@ -3,6 +3,7 @@
 import numpy
 import argparse
 import re
+import numpy as np
 
 # handle input argument - the input file path
 parser = argparse.ArgumentParser()
@@ -28,12 +29,22 @@ while '==================' not in line:
     xyz += line
     line = f.readline()
 
-# print(xyz)
-#print(re.findall('-?\d\.\d*', xyz))
+# get the xyz coords from the .xyz block
 nums = [float(s) for s in re.findall('-?\d\.\d*', xyz)]
-# print(nums)
-for i in range(0, len(nums)):
-    print(nums[i])
+
+# arrange them into a triplet for each molecule
+pos = [[nums[x], nums[x+1], nums[x+2]] for x in range(0, len(nums), 3)]
+
+# find the pairwise distances (15 C 2 = 105)
+for i in range(0, len(pos)):
+    for p in range(i+1, len(pos)):
+        pos1 = np.array(pos[i])
+        pos2 = np.array(pos[p])
+        dist = np.linalg.norm(pos1-pos2) 
+
+
+
+# close the file
 f.close()
 
 
