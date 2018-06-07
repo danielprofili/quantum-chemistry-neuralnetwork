d71a50466380b15de586ab96ff687862223bd9fd
danielprofili <dprofili@gmail.com>
2018-06-07 11:19:35 -0400

    Reorganize

--- tf/generate_input.py
+++ tf/generate_input.py
@@ -1,15 +1,40 @@
-# Generate the input vector from the input file
-# 5 June 2018
-import numpy
-import argparse
-import re
-import numpy as np
+from get_coords import get_coords
 
+
+# get_coords: parse the pairwise distances between each atom
+# returns a tuple containing (1) an array of the atom pair tuples and (2) an 
+# array of the pairwise distances
+
+def get_coords(xyz):
+# get the xyz coords from the .xyz block
+    nums = [float(s) for s in re.findall('-?\d\.\d*', xyz)]
+    elems = [s for s in re.findall('[A-Z]', xyz)]
+
+# arrange them into a triplet for each molecule
+    pos = [[nums[x], nums[x+1], nums[x+2]] for x in range(0, len(nums), 3)]
+    tup_list = []
+    dist_list = []
+# find the pairwise distances (angstroms) (15 atoms choose 2 = 105)
+    for i in range(0, len(pos)):
+        for p in range(i+1, len(pos)):
+            pos1 = np.array(pos[i])
+            pos2 = np.array(pos[p])
+            dist = np.linalg.norm(pos1-pos2) 
+            # create a tuple matching the first element to the second element
+            elems_tup = (elems[i] + str(i+1),elems[p] + str(p+1))
+            
+            # create a tuple of the elements and the distances
+            tup_list = tup_list + tup
+            dist_list = dist_list + dist
+
+    return (tup_list, dist_list)
+# begin the main method
 
 # handle input argument - the input file path
 parser = argparse.ArgumentParser()
 parser.add_argument("inputfile", help="path to the input file")
 args = parser.parse_args()
+
 
 # set up regex parsing object for parsing the molecule name (TFSI_###_###)
 reg = re.compile('TFSI_[0-9]{1,3}_[0-9]{1,3}\.gzmat')
@@ -27,27 +52,7 @@ while '==================' not in line:
     xyz += line
     line = f.readline()
 
-# get the xyz coords from the .xyz block
-nums = [float(s) for s in re.findall('-?\d\.\d*', xyz)]
-elems = [s for s in re.findall('[A-Z]', xyz)]
-
-# arrange them into a triplet for each molecule
-pos = [[nums[x], nums[x+1], nums[x+2]] for x in range(0, len(nums), 3)]
-
-# find the pairwise distances (15 C 2 = 105)
-for i in range(0, len(pos)):
-    for p in range(i+1, len(pos)):
-        pos1 = np.array(pos[i])
-        pos2 = np.array(pos[p])
-        dist = np.linalg.norm(pos1-pos2) 
-        # create a tuple matching the first element to the second element
-        elems_tup = (elems[i] + str(i+1),elems[p] + str(p+1))
-        
-        # create a tuple of the elements and the distances
-        tup = (elems_tup, dist)
 
         
 # close the file
 f.close()
-
-
