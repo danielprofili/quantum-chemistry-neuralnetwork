6c2f1ef0264883781973f008970771084b8740a3
danielprofili <dprofili@gmail.com>
2018-08-21 20:17:38 -0400

    Modify more stuff

--- keras/NN_qc_fit_main.py
+++ keras/NN_qc_fit_main.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 import numpy as np
 import routines2 as routines
 import generate_input as gen
-from FFenergy_openMM import compute_SAPT_FF_energies
+#from FFenergy_openMM import compute_SAPT_FF_energies
 import symmetry_functions as sym
 from force_field_parameters import *
 from construct_NN import construct_NN_Model
@@ -20,13 +20,13 @@ inputfile='input.qc'
 #print("NN will use bohr units consistent with input coordinates")
 
 # compute SAPT-FF energies
-energy_SAPTFF = compute_SAPT_FF_energies( xyz , 'h2o_template.pdb', 'water_residue.xml' , 'water_sapt.xml' )
+#energy_SAPTFF = compute_SAPT_FF_energies( xyz , 'h2o_template.pdb', 'water_residue.xml' , 'water_sapt.xml' )
 
 #for i in range(len(energy_SAPTFF)):
 #    print( energy[i]*2.6255, energy_SAPTFF[i] )
 
 # fit to the residual, (kJ/mol)
-energy_fit = energy * 2.6255 - np.array( energy_SAPTFF )
+#energy_fit = energy * 2.6255 - np.array( energy_SAPTFF )
 
 # load the Neural network force field definitions
 NNff = NNforce_field( 'FF3' )
@@ -42,8 +42,8 @@ model = construct_NN_Model( sym_input, NNff, aname )
 
 model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
 
-print( "fitting Neural network to interaction energy data...")
-history=model.fit( sym_input , energy_fit, batch_size=20, epochs=5000, validation_split=0.1 )
+print( "fitting Neural network to charge data...")
+history=model.fit( sym_input , charges, batch_size=20, epochs=5000, validation_split=0.1 )
 print( "done fitting Neural network" )
  
 print( "summary" )
