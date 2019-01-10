e1aab9a12ff718147a3a3d763ae47ee2c2c56807
danielprofili <dprofili@gmail.com>
2019-01-10 18:32:07 -0500

    Fix issue where failed computations were included in the input file

--- quantum_chemistry/nn_input.sh
+++ quantum_chemistry/nn_input.sh
@@ -13,18 +13,23 @@ echo "BEGIN INPUT FILE" > $working_dir/input
 cd subs
 for f in *
 do
-# f is the folder (i.e. the perturbation name TFSI_xxx_xx)
-    #echo $f 
-    # Print the name at the top of the block
-    cd $working_dir
-    echo -e "\n$f\n" >> $working_dir/input
-    cat xyz/$f.xyz >> $working_dir/input
-    echo -e "\n===================\n" >> $working_dir/input
-    cat gzmat/$f.gzmat >> $working_dir/input
-    echo -e "\n===================\n" >> $working_dir/input
-    echo -e "Begin charges\n" >> $working_dir/input
-    cat $working_dir/subs/$f/$f.charges >> $working_dir/input
-    echo -e "\n===================\n" >> $working_dir/input
-    #echo DEBUG
-    #sleep 5
+        # put the check for success here
+        # f is the folder (i.e. the perturbation name TFSI_xxx_xx)
+        #echo $f 
+        # Print the name at the top of the block
+        cd $working_dir
+        flag=`grep "Psi4 exiting" ${working_dir}/subs/${f}/${f}.out | wc | cut -c -10` 
+        if [ $flag -eq 1 ] 
+        then 
+                echo -e "\n$f\n" >> $working_dir/input
+                cat xyz/$f.xyz >> $working_dir/input
+                echo -e "\n===================\n" >> $working_dir/input
+                cat gzmat/$f.gzmat >> $working_dir/input
+                echo -e "\n===================\n" >> $working_dir/input
+                echo -e "Begin charges\n" >> $working_dir/input
+                cat $working_dir/subs/$f/$f.charges >> $working_dir/input
+                echo -e "\n===================\n" >> $working_dir/input
+        fi
+        #echo DEBUG
+        #sleep 5
 done
